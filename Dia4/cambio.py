c0d62b1df18f7056fdc6385185c7c5a719b54a50
yaritza <yaritzagamboa511gmail.com>
2024-04-13 13:38:20 -0500

    yaritza

--- Dia4/cambio.py
+++ Dia4/cambio.py
@@ -1,26 +1,29 @@
-def cambio_de_monedas_total(monto_dar):
-#calcular numero minimo de monedas 
-    monedas = [1, 5, 10]
-    result=[]
-    total_monedas =0 # el total de monedas
-    for m in reversed(monedas): #rodemamos las monedas de mayor a menor  denominacion
-        cant_monedas=monto_dar // m # mientras el moto a cambiar sea mayor o igual a la moneda actual
-        result.append((m, cant_monedas))# resultado de moneda
-        total_monedas+= cant_monedas 
-        monto_dar %= m #el valor de la moneda al monto al cambiar
-#ver el resultado
-    print("numero minimo de monedas:") 
-    print(total_monedas)
-    print("detalle de monedas")
-    for moneda, cantidad in result:
-        print(f"{cantidad} moneda(s) de {moneda}")
-    print("suma total de monedas utilizadas:")
-    print(sum(cantidad for_, cantidad in result))
-    # solicitar el monto al usuario
-monto_dar=int(input("ingrese el monto a cambiar:"))
-#  llamar la funcion para cvalcular el cambio
-cambio_de_monedas_total(monto_dar)
-         
+def cambio_de_moneda(dinero):
+    denominaciones= [10,5,1]
+    resultado= [0,0,0]
+    for i, denominacion in enumerate(denominaciones):
+        num_monedas= dinero // denominacion
+        resultado[i] = num_monedas
+        dinero-= num_monedas* denominacion
+        
+    return resultado,sum(resultado)
+#  el uso de monedas ejemplo 
+dinero = int(input("ingrese la cantidad de dinero:"))
+monedas_necesarias, suma_total= cambio_de_moneda (dinero)
+print("monedas necesarias de denominacion 10:",monedas_necesarias[0])
+print("monedas necesarias de denominacion 5:",monedas_necesarias[1])
+print("monedas necesarias de denominacion 1:",monedas_necesarias[2])
+print("suma total de monedas:",suma_total)
+
+
+
+      
+
+
+
+
+ 
+
 
 
 
